5aab8338ce0fc26cb37039c314056ae5c12a0439
avances123 <avances123@gmail.com>
2010-03-14 22:54:27 +0100

    Creando el modelo de rayos

--- geometeo/rayos/models.py
+++ geometeo/rayos/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+#from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+class rayos_ukmo(models.Model):
+	lon = models.FloatField()
+	lat = models.FloatField()
+	fecha = models.DateTimeField()
+
+	#especifico de geodjango
+	geopunto = models.PointField()
+	objects = models.GeoManager()
+
+# Returns the string representation of the model.
+def __unicode__(self):
+        return self.name
+
