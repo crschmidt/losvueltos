ff26a56dbde86f90aed9aeada23e10f3b0110369
avances123 <avances123@gmail.com>
2010-06-18 23:36:32 +0200

    Entendiendo render_to_response y JSON

--- geometeo/rayos/views.py
+++ geometeo/rayos/views.py
@@ -9,7 +9,19 @@ from django.shortcuts import render_to_response
 from django.template import Template, Context
 from django.http import HttpResponse
 
+# vectorformats
+from vectorformats.Formats import Django, GeoJSON
+from rayos.models import rayos_ukmo
+
 
 def pinta_rayos(request):
-    return render_to_response('rayos.html')
+    json = calcula_json()
+    return render_to_response('rayos-json.html')
+
+def calcula_json():
+    qs = rayos_ukmo.objects.filter(error__gt=5)
+    djf = Django.Django(geodjango="geopoint", properties=['error', 'lat','lon'])
+    geoj = GeoJSON.GeoJSON()
+    string = geoj.encode(djf.decode(qs))
+    return string
 
