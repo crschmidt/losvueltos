52e68c755500a9c0ff8e1df65f0de9ab0bc16c16
Christopher Schmidt <crschmidt@crschmidt.net>
2010-06-18 17:58:58 -0400

    fix view

--- geometeo/rayos/views.py
+++ geometeo/rayos/views.py
@@ -15,10 +15,9 @@ from rayos.models import rayos_ukmo
 
 
 def pinta_rayos(request):
-    json = calcula_json()
     return render_to_response('rayos-json.html')
 
-def calcula_json():
+def calcula_json(request):
     qs = rayos_ukmo.objects.filter(error__gt=5)
     djf = Django.Django(geodjango="geopoint", properties=['error', 'lat','lon'])
     geoj = GeoJSON.GeoJSON()
